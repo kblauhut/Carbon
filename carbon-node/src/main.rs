dff1cfd5f2987d206dd5e098080fd39d9e40c3db
kblauhut <koljablauhut@gmail.com>
2021-01-11 18:03:53 +0100

    -Added functionality to send new ips to peers

--- carbon-node/src/main.rs
+++ carbon-node/src/main.rs
@@ -1,29 +1,52 @@
 use std::io::prelude::*;
 use std::io::{self, Read};
-use std::net::{TcpStream, SocketAddr, IpAddr, TcpListener, Shutdown};
+use std::net::{TcpStream, SocketAddr, IpAddr, TcpListener, Shutdown, Ipv4Addr};
+
+use rand::prelude::*;
 
 const MAX_PEERS:u8 = 16;
 const PING:u8 = 0x00;
 const PONG:u8 = 0x01;
+const REQ_PEER:u8 = 0x02;
+const SND_PEER:u8 = 0x03;
 
 fn main() -> std::io::Result<()> {
-    let mut peers: Vec<&str> = vec![];
+    let mut peers: Vec<SocketAddr> = vec![];
+    peers.push(SocketAddr::new(IpAddr::V4(Ipv4Addr::new(192, 168, 2, 1)), 7878));
+    peers.push(SocketAddr::new(IpAddr::V4(Ipv4Addr::new(192, 168, 2, 6)), 7878));
+    println!("{:?}", peers);
 
     let listener = TcpListener::bind("0.0.0.0:7878")?;
-    connect_peer("192.168.2.118:5000", &[0x48, 0x65, 0x6c, 0x6c, 0x6f]);
+
+    peer_connect("192.168.2.118:5000", &mut peers, REQ_PEER);
     for stream in listener.incoming() {
-        handle_client(stream?);
+        handle_client(stream?, &peers);
     }
     Ok(())
 }
 
-fn handle_client(mut stream: TcpStream) {
-    net_msg_enc(stream.local_addr().unwrap());
+fn handle_client(mut stream: TcpStream, peers: &Vec<SocketAddr>) {
+    snd_peer_enc(stream.local_addr().unwrap());
 
     let mut buf = [0; 1024];
+    let mut res = [0u8; 128];
     while match stream.read(&mut buf) {
         Ok(size) if size > 0 => {
-            stream.write(&buf[0..size]).unwrap();
+            match buf[0] {
+                PING => res[0] = PONG,
+                REQ_PEER => {
+                    let mut rng = thread_rng();
+                    let rand = rng.gen_range(0..peers.len()-1);
+                    let ip_enc = snd_peer_enc(peers[rand]);
+
+                    res[0] = SND_PEER;
+                    for (i) in (0..ip_enc.len()) {
+                        res[1+i] = ip_enc[i];
+                    }
+                },
+                _ => ()
+            }
+            stream.write(&res).unwrap();
             println!("{}", stream.peer_addr().unwrap());
             true
         },
@@ -36,10 +59,18 @@ fn handle_client(mut stream: TcpStream) {
     } {}
 }
 
-fn connect_peer(addr: &str, message: &[u8]) -> std::io::Result<()> {
-    let mut buf = [0; 128]; // just a placeholder
+fn peer_connect(addr: &str, peers: &mut Vec<SocketAddr>, msg_type: u8) -> std::io::Result<()> {
+    let mut buf = [0u8; 128]; // just a placeholder
+    let mut pkt_bytes = [0u8; 128]; // just a placeholder
     let mut stream = TcpStream::connect(addr)?;
-    stream.write(&message)?;
+
+    match msg_type {
+        PING => pkt_bytes[0] = PING,
+        REQ_PEER => pkt_bytes[0] = REQ_PEER,
+        _ => ()
+    }
+
+    stream.write(&pkt_bytes)?;
     stream.read(&mut buf);
     
     println!("{:?}", buf);
@@ -47,11 +78,7 @@ fn connect_peer(addr: &str, message: &[u8]) -> std::io::Result<()> {
     Ok(())
 }
 
-fn msg_send() {
-
-}
-
-fn net_msg_enc(addr: SocketAddr) {
+fn snd_peer_enc(addr: SocketAddr) -> [u8; 18] {
     let mut msg = [0u8; 18];
     match addr.ip() {
         IpAddr::V4(ip) => {
@@ -70,4 +97,6 @@ fn net_msg_enc(addr: SocketAddr) {
     msg[16] = (addr.port() >> 8) as u8;
     msg[17] = addr.port() as u8;
     println!("{:X?}", msg);
+
+    return msg;
 }
